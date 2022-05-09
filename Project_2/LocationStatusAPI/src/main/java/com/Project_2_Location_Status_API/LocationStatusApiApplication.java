9b0fb5d1a34b2998e0564fc0cc2f17a19bec00cf
alahkaih <alahkaihJandrus@gmail.com>
2022-05-09 14:50:43 -0700

    Fixed the APIs to be ran

---
+++ Project_2/LocationStatusAPI/src/main/java/com/Project_2_Location_Status_API/LocationStatusApiApplication.java
@@ -0,0 +1,13 @@
+package com.Project_2_Location_Status_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationStatusApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationStatusApiApplication.class, args);
+    }
+
+}
