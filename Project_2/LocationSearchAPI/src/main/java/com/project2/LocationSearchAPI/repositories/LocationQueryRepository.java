f999453b21c7693d4413cabaf66319940eeb0e29
HarrisonFok <harrison.fok@gmail.com>
2022-05-10 21:04:29 -0400

    Ft 2/create repo entity controller service for search api (#18)

---
+++ Project_2/LocationSearchAPI/src/main/java/com/project2/LocationSearchAPI/repositories/LocationQueryRepository.java
@@ -0,0 +1,27 @@
+package com.project2.LocationSearchAPI.repositories;
+
+import com.project2.LocationSearchAPI.entities.LocationQuery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LocationQueryRepository extends JpaRepository<LocationQuery, Integer> {
+    @Query("FROM LocationQuery WHERE locationName = :locationName")
+    List<LocationQuery> findAllByLocationName(@Param("locationName") String locationName);
+    @Query("FROM LocationQuery WHERE status = :status")
+    List<LocationQuery> findAllByStatus(@Param("status") String status);
+    @Query("FROM LocationQuery WHERE population >= :population")
+    List<LocationQuery> findAllByPopulation(@Param("population") int population);
+    @Query("FROM LocationQuery WHERE vaccinated >= :vaccinated")
+    List<LocationQuery> findAllByVaccinatedPopulation(@Param("vaccinated") int vaccinated);
+    @Query("FROM LocationQuery WHERE totalInfections >= :totalInfections")
+    List<LocationQuery> findAllByNumInfections(@Param("totalInfections") int totalInfections);
+    @Query("FROM LocationQuery WHERE totalDeaths >= :totalDeaths")
+    List<LocationQuery> findAllByTotalDeaths(@Param("totalDeaths") int totalDeaths);
+    @Query("FROM LocationQuery WHERE totalRecovered >= :totalRecovered")
+    List<LocationQuery> findAllByTotalRecovered(@Param("totalRecovered") int totalRecovered);
+}
---
+++ Project_2/LocationSearchAPI/src/test/java/com/project2/LocationSearchAPI/LocationSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2.LocationSearchAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
