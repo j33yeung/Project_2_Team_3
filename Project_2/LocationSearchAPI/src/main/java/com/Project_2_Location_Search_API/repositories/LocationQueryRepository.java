32344a0a0a60c733fe9c0db2622cf44d4a670b20
HarrisonFok <harrison.fok@gmail.com>
2022-05-15 11:31:27 -0400

    Fixed the bug where POSTing LocationQuery was suddenly not working anymore (#56)

--- Project_2/LocationSearchAPI/src/main/java/com/Project_2_Location_Search_API/repositories/LocationQueryRepository.java
+++ Project_2/LocationSearchAPI/src/main/java/com/Project_2_Location_Search_API/repositories/LocationQueryRepository.java
@@ -12,18 +12,8 @@ import java.util.List;
 @Repository
 public interface LocationQueryRepository extends JpaRepository<LocationQuery, Integer> {
 
-//    These can be used to replace LocationName and status, or get a specific value from one of the ints
-//    ArrayList<LocationQuery> findAllByLocationName(String location_name);
-//    ArrayList<LocationQuery> findAllByPopulation(int population);
-//    ArrayList<LocationQuery> findAllByStatus(String Status);
-//    ArrayList<LocationQuery> findAllByTotalDeaths(int total_deaths);
-//    ArrayList<LocationQuery> findAllByTotalRecovered(int total_recovered);
-//    ArrayList<LocationQuery> findAllByTotalInfections(int total_infections);
-//    ArrayList<LocationQuery> findAllByVaccinated(int vaccinated);
-
-
-    @Query("FROM LocationQuery WHERE locationName = :location_name")
-    List<LocationQuery> findAllByLocationName(@Param("location_name") String locationName);
+    @Query("FROM LocationQuery WHERE location_name = :location_name")
+    List<LocationQuery> findAllByLocationName(@Param("location_name") String location_name);
 
     @Query("FROM LocationQuery WHERE status = :status")
     List<LocationQuery> findAllByStatus(@Param("status") String status);
@@ -34,12 +24,12 @@ public interface LocationQueryRepository extends JpaRepository<LocationQuery, In
     @Query("FROM LocationQuery WHERE vaccinated >= :vaccinated")
     List<LocationQuery> findAllByVaccinatedPopulation(@Param("vaccinated") int vaccinated);
 
-    @Query("FROM LocationQuery WHERE totalInfections >= :total_infections")
-    List<LocationQuery> findAllByTotalInfections(@Param("total_infections") int totalInfections);
+    @Query("FROM LocationQuery WHERE total_infections >= :total_infections")
+    List<LocationQuery> findAllByTotalInfections(@Param("total_infections") int total_infections);
 
-    @Query("FROM LocationQuery WHERE totalDeaths >= :total_deaths")
-    List<LocationQuery> findAllByTotalDeaths(@Param("total_deaths") int totalDeaths);
+    @Query("FROM LocationQuery WHERE total_deaths >= :total_deaths")
+    List<LocationQuery> findAllByTotalDeaths(@Param("total_deaths") int total_deaths);
 
-    @Query("FROM LocationQuery WHERE totalRecovered >= :total_recovered")
-    List<LocationQuery> findAllByTotalRecovered(@Param("total_recovered") int totalRecovered);
+    @Query("FROM LocationQuery WHERE total_recovered >= :total_recovered")
+    List<LocationQuery> findAllByTotalRecovered(@Param("total_recovered") int total_recovered);
 }
